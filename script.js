2cbeac7d2a95de6fd9e7334aaeaa251c02055cf2
lirow150 <lorena.paez0814@gmail.com>
2022-07-06 20:51:24 -0500

    changes made

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+    response.json().then(function(json) {
+    //loop
+        <div class="astronaut">
+        <div class="bio">
+           <h3>${json.firstName} ${jason.lastName}</h3>
+           div.inneerHTML = `
+           <ul>
+              <li>Hours in space: ${json.hoursInSpace}</li>
+              <li>Active: ${json.active}</li>
+              <li>Skills: ${json.skills}</li>
+           </ul>
+           `;
+        </div>
+        <img class="avatar" src="${json.picture}">
+     </div>
+
